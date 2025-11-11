26cb50f6d3ccadc52a79685f1f8d85f54735475a
Arif Hasan <arifhasanah2002@gmail.com>
2025-11-12 03:38:01 +0600

    dash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:link_unity/home_page.dart';
-import 'package:link_unity/student/student_dash.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-      home: StudentDashboard(),
+      home: HomePage(),
   ));
 }
