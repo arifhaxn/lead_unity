e0404f87464710bedac4c49cb8e11294ee812aa1
Omio-Mahim <omiomahim@gmail.com>
2026-02-27 15:09:09 +0600

    project details page

--- lib/widgets/about_app_screen.dart
+++ lib/widgets/about_app_screen.dart
@@ -45,7 +45,7 @@ class AboutAppScreen extends StatelessWidget {
         "name": "Omio Mahim",
         "batch": "61st Batch",
         "department": "Dept. of CSE",
-        "role": "Frontend Developer (AI Feature Integration)",
+        "role": "Frontend Developer & AI Feature Integrator",
         "image": "assets/template/crew/omio.jpeg",
         "fb": "https://www.facebook.com/omio.mahim.044",
         "linkedin": "https://www.linkedin.com/in/omio-mahim-79a2302a8",
